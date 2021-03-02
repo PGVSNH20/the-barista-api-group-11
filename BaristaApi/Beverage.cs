db9fb11d141b76e511f91947b47be01eb31abb44
Martin Berntson <temporal@live.se>
2021-03-02 15:32:59 +0100

    Committed

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -1,20 +1,107 @@
+using System;
 using System.Collections.Generic;
 
 public interface IBeverage{
-	List<string> Ingredients { get; }
+	List<Ingredient> Ingredients { get; }
     string CupType { get; }
+    IBeverage AddIngredient(Ingredient newIngredient);
+
+    public IBeverage WhatIsThis();
+
 }
 
-class Espresso : IBeverage
+class GreenTea : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
-
-    public string CupType => throw new System.NotImplementedException();
+    public List<Ingredient> Ingredients { get; set; }
+    public string CupType => this.GetType().ToString();
+    public GreenTea()
+    {
+        Ingredients = new List<Ingredient>();
+    }
+    public IBeverage AddIngredient(Ingredient newIngredient)
+    {
+        this.Ingredients.Add(newIngredient);
+        return this;
+    }
+    public IBeverage WhatIsThis()
+    {
+        Console.WriteLine($"A cup of {this.CupType}.");
+        foreach (Ingredient ingredient in Ingredients)
+        {
+            Console.WriteLine($"\twith some {ingredient.Amount} of the {ingredient.Type}");
+        }
+        return this;
+    }
 }
 
-class Latte : IBeverage
+class Juice : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+    public List<Ingredient> Ingredients { get; set; }
+    public string CupType => this.GetType().ToString();
+    public Juice()
+    {
+        Ingredients = new List<Ingredient>();
+    }
+    public IBeverage AddIngredient(Ingredient newIngredient)
+    {
+        this.Ingredients.Add(newIngredient);
+        return this;
+    }
+    public IBeverage WhatIsThis()
+    {
+        Console.WriteLine($"A cup of {this.CupType}.");
+        foreach (Ingredient ingredient in Ingredients)
+        {
+            Console.WriteLine($"\twith some {ingredient.Amount} of the {ingredient.Type}");
+        }
+        return this;
+    }
+} 
 
-    public string CupType => throw new System.NotImplementedException();
+
+class Coffee : IBeverage
+{
+    public List<Ingredient> Ingredients { get; set; }
+    public string CupType => this.GetType().ToString();
+    public Coffee()
+    {
+        Ingredients = new List<Ingredient>();
+    }
+    public Coffee AddWater(Ingredient newIngredient)
+    {
+        if (newIngredient.Type.ToLower() == "water")
+            this.Ingredients.Add(newIngredient);
+        return this;
+    }
+    public Coffee AddBeans(Ingredient newIngredient)
+    {
+        if (newIngredient.Type.ToLower() == "beans")
+            this.Ingredients.Add(newIngredient);
+        return this;
+    }
+    public IBeverage AddIngredient(Ingredient newIngredient)
+    {
+        this.Ingredients.Add(newIngredient);
+        return this;
+    }
+    public IBeverage WhatIsThis()
+    {
+        Console.WriteLine($"A cup of {this.CupType}.");
+        foreach (Ingredient ingredient in Ingredients)
+        {
+            Console.WriteLine($"\twith some {ingredient.Amount} of the {ingredient.Type}");
+        }
+        return this;
+    }
+}
+
+public class Ingredient
+{
+    public Ingredient(int amount, string type)
+    {
+        Amount = amount;
+        Type = type;
+    }
+    public int Amount { get; set; }
+    public string Type { get; set; }
 }
--- BaristaApi/Program.cs
+++ BaristaApi/Program.cs
@@ -6,6 +6,20 @@ namespace BaristaApi
     {
         static void Main(string[] args)
         {
+            IBeverage newCup = new Juice();
+            newCup.AddIngredient(new Ingredient(20, "water"))
+                .AddIngredient(new Ingredient(5, "apple")).WhatIsThis();
+            IBeverage secondCup = new Coffee()
+                                    .AddWater(new Ingredient(10, "Water"))
+                                    .AddBeans(new Ingredient(5, "beans"))
+                                    .AddIngredient(new Ingredient(5, "milk"))
+                                    .WhatIsThis();
+            IBeverage starbucksCoffee = new Coffee()
+                                    .AddWater(new Ingredient(10, "Water"))
+                                    .AddBeans(new Ingredient(5, "beans"))
+                                    .AddIngredient(new Ingredient(5, "milk"))
+                                    .AddIngredient(new Ingredient(500, "sugar"))
+                                    .WhatIsThis();
             // Test push this
             /* pseudo-code
             Espresso espresso = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 5 && b.Sort = CoffeSorts.Robusta).ToBravage();
@@ -14,11 +28,6 @@ namespace BaristaApi
             Latte latte = new Espresso().AddWater(20).AddBeans(b => b.AmountInG = 7 && b.Sort = CoffeSorts.Robusta).AddMilk().ToBravage();
             //latte is type of Latte
              */
-
-            Console.Write("Pingas");
-
-            //smurf
         }
-    }
     }
 }
