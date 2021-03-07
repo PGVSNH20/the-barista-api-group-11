fb6db5f8c4e72645d558dbdc21bfd60743a20489
Martin Berntson <temporal@live.se>
2021-03-07 13:13:47 +0100

    Added tests. Three "check that the machine makes the right type of beverage" tests and two different styles (hard-coded and dynamic) of "see if a specific ingredient is in the ingredient list" tests. Should be enough.

--- BaristaApi.Test/FluentApiTests.cs
+++ BaristaApi.Test/FluentApiTests.cs
@@ -5,7 +5,7 @@ namespace BaristaApi.Tests
     public class FluentApiTests 
     {
         [Fact]
-        public void When_AddingWaterAndBeans_Expect_Espresso(){
+        public void When_Using_CoffeeMaker_Expect_Coffee(){
             //Pseudo code
             var coffeMachine = new CoffeeMaker();
             var beverage = coffeMachine
@@ -15,6 +15,63 @@ namespace BaristaApi.Tests
                             .FinishBrewing();
             // Assert
             Assert.Equal("Coffee", beverage.CupType);
+        }
+
+        [Fact]
+        public void When_Using_JuicePress_Expect_Juice()
+        {
+            var juicePress = new JuicePress();
+            var beverage = juicePress
+                            .StartMixingJuice()
+                            .AddOrange()
+                            .FinishMixing();
+            Assert.Equal("Juice", beverage.CupType);
+        }
+
+        [Fact]
+        public void When_Using_MilkshakeMachine_Expect_Milkshake()
+        {
+            var milkshakeMachine = new MilkshakeMachine();
+            var beverage = milkshakeMachine
+                            .Shaker()
+                            .AddIceCream("Strawberry")
+                            .AddALotOfMilk("Chocolate")
+                            .FinishShaking();
+            Assert.Equal("Milkshake", beverage.CupType);
+        }
+        [Fact]
+        public void When_Adding_Milk_To_Coffee_Expect_Milk_In_Ingredients_List()
+        {
+            var coffeMachine = new CoffeeMaker();
+            var beverage = coffeMachine
+                            .StartBrewingCoffee()
+                            .AddWater()
+                            .AddBeans()
+                            .AddALotOfMilk()
+                            .FinishBrewing();
+            Assert.True(beverage.Ingredients[2].Type.ToLower() == "milk");
+        }
+
+        [Fact]
+        public void Expect_Apple_In_Ingredients_When_Adding_Apple_To_Juice()
+        {
+            var juicePress = new JuicePress();
+            var beverage = juicePress
+                            .StartMixingJuice()
+                            .AddOrange()
+                            .AddApple()
+                            .AddWater()
+                            .FinishMixing();
+            var containsApple = false;
+            foreach (var ingredient in beverage.Ingredients)
+            {
+                if (!containsApple)
+                {
+                    if (ingredient.Type.ToLower() == "apple")
+                        containsApple = true;
+                }
+            }
+            Assert.True(containsApple);
         }
     }
 }
